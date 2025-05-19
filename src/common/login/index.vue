762e6bbaf4f991580f28678fd9d6f3c840f3cd16
richu <1691401076@qq.com>
2025-05-19 20:45:27 +0800

    core:add icon style

--- src/common/login/index.vue
+++ src/common/login/index.vue
@@ -1,8 +1,16 @@
 <template>
-    <p>login</p>
+    <CommonBar>
+        <s-scroll-view :class="{ main: true, main_menu: !menuStore.isflod }">
+            <s-text-field label="用户名或邮箱"></s-text-field>
+            <s-text-field label="密码"></s-text-field>
+        </s-scroll-view>
+    </CommonBar>
 </template>
 
 <script setup lang="ts">
+import CommonBar from '@/components/CommonBar/index.vue'
+import MenuStore from '@/store/modules/menu';
+let menuStore  = MenuStore()
 
 </script>
 
