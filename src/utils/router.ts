81c36f3c3096c7ae3218fd6bf1cb2bf2d8f7410c
plana <13058160568@163.com>
2025-05-27 20:08:44 +0800

    fixed:change the router

--- src/utils/router.ts
+++ src/utils/router.ts
@@ -1,13 +1,22 @@
 import type { Router } from 'vue-router'
+import MenuStore from '@/store/modules/menu';
+let menuStore = MenuStore()
+
+function change(){
+    menuStore.isflod = false
+}
 
 export const ToHome = ($router:Router) => {
     $router.push("/home");
+    change()
 }
 
 export const ToLogin = ($router:Router) => {
     $router.push("/login");
+    change()
 }
 
 export const ToRegister = ($router:Router) => {
     $router.push("/register");
+    change()
 }
