d1419085645a49dfe2ed9fd7df8f10a93f967094
richuff <1691401076@qq.com>
2025-05-27 20:15:46 +0800

    fixed:update components

--- src/page/home/index.vue
+++ src/page/home/index.vue
@@ -1,6 +1,6 @@
 <template>
     <CommonBar>
-        <s-scroll-view @scroll="scroll" :class="{ main: true, main_menu: !menuStore.isflod }">
+        <s-scroll-view @scroll="scroll" :class="{main: true, main_menu: !menuStore.isflod }">
                 <s-card :clickable="true" class="card">
                     <div slot="image">
                     </div>
@@ -47,20 +47,25 @@
                     <div slot="subhead">Subhead</div>
                     <div slot="text">Cards are often used for grid lists, which provide click effects</div>
                 </s-card>
-                <BottomBar></BottomBar>
+            <BottomBarWithFixed></BottomBarWithFixed>
         </s-scroll-view>
     </CommonBar>
 </template>
 
 <script setup lang="ts">
 import CommonBar from '@/components/CommonBar/index.vue'
-import BottomBar from '@/components/BottomBar/index.vue'
+import BottomBarWithFixed from '@/components/BottomBarWithFixed/index.vue'
 import { scroll } from '@/utils/scroll';
 import MenuStore from '@/store/modules/menu';
 let menuStore  = MenuStore()
-
 </script>
 
 <style lang="less">
-
+@import url('../../assets/style.less');
+.main{
+    s-card {
+        margin-bottom: 0.3rem;
+    }
+    margin-bottom: calc(@bottom-height + 1rem);
+}
 </style>
