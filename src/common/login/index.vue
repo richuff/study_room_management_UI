3eeb019310122d3200208a225d99c38b5df1f442
richuff <1691401076@qq.com>
2025-10-13 10:54:17 +0800

    更改样式

--- src/common/login/index.vue
+++ src/common/login/index.vue
@@ -4,17 +4,20 @@
             <div class="main_bar" >
                 <div class="login" style="padding: 1.67rem;">
                     <div class="title">登录账号</div>
-                    <s-text-field @input="setEmail($event as InputEvent)" label="用户名或邮箱"></s-text-field>
+                    <s-text-field @input="setEmail($event as InputEvent)" label="邮箱"></s-text-field>
                     <s-text-field  @input="setPassword($event as InputEvent)" label="密码"></s-text-field>
                     <div class="forget-pass">
-                        <a href="">忘记密码</a>
+                        <span class="forget-pass-span" @click="ToForgetPass($router)">忘记密码</span>
                     </div>
-                    <s-button class="button" @click="Login(dataForm)">开始存档</s-button>
+                    <s-button class="button" :disabled="isDisable()" @click="BeginLogin()">开始存档</s-button>
                     <s-button class="button" @click="ToRegister($router)">还没有账户?戳我注册</s-button>       
                     <s-divider style="margin-top: 1.8rem;">使用第三方登录</s-divider>
                 </div>
                 <div class="other_login">
-                    <i class="iconfont icon-erweima"></i>
+                    <s-tooltip>
+                        <i slot="trigger" class="iconfont icon-erweima"></i>
+                        <img src="/public/BG_Spring.jpg" style="max-width: 8rem">
+                    </s-tooltip>
                 </div>
             </div>
             <BottomBarWithFixed></BottomBarWithFixed>
@@ -26,13 +29,17 @@
 import CommonBar from '@/components/CommonBar/index.vue'
 import BottomBarWithFixed from '@/components/BottomBarWithFixed/index.vue'
 import MenuStore from '@/store/modules/menu';
-import { ToRegister } from '@/utils/router';
+import { ToRegister,ToHome,ToForgetPass } from '@/utils/router';
 import { useRouter } from 'vue-router';
-import { Login } from '@/api/user/api';
 import type { UserData } from '@/api/user/type';
-let menuStore  = MenuStore()
-let $router = useRouter()
+import { showError,showSuccess } from '@/widget/SnackBar'
 import { onMounted,reactive } from 'vue';
+import UserStore from '@/store/modules/user';
+import { isEmail } from '@/utils/email';
+let menuStore  = MenuStore()
+let userStore = UserStore()
+let $router = useRouter()
+
 
 let dataForm:UserData = reactive<UserData>({
     email:"",
@@ -57,7 +64,29 @@ const setPassword = (i: InputEvent) => {
     const item = i.target as HTMLInputElement
     dataForm.password = item.value
 }
+const BeginLogin = () => {
+    if (dataForm.password.length > 40 || dataForm.email.length > 40){
+        showError("密码或邮箱过长")
+        return
+    }
+    if (!isEmail(dataForm.email)){
+        showError("邮箱格式错误")
+        return
+    }
+    userStore.UserLogins(dataForm).then(()=>{
+        showSuccess("登录成功")
+        ToHome($router)
+    }).catch((reason:any)=>{
+        showError(reason)
+    })
+}
 
+const isDisable = () => {
+    if (dataForm.email != "" && dataForm.password != ""){
+        return false
+    }
+    return true
+}
 </script>
 
 <style lang="less" scoped>
