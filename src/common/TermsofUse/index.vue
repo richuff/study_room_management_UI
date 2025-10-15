f369411ae11550e03568eed740500faa655207d2
richuff <1691401076@qq.com>
2025-10-15 09:26:56 +0800

    新增页面

---
+++ src/common/TermsofUse/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <CommonBar>
+        <div>setting</div>
+    </CommonBar>
+</template>
+
+<script setup lang="ts">
+import CommonBar from '@/components/CommonBar/index.vue'
+
+</script>
+
+<style lang="less" scoped>
+@import url('./index.less');
+</style>
